5990ee33556fdab5fe987179e8630cf4cc3b0f02
K.Venketesh Dora <kvenketeshdora@gmail.com>
2024-02-10 21:21:15 +0530

    Modified

--- oops.java
+++ oops.java
@@ -1,7 +1,10 @@
 class calculate{
     public int add(int a, int b){
-        int addition = a + b;
-        return addition;
+        int sum = a + b;
+        return sum;
+    }
+    public void print(){
+        System.out.println("Hello world");
     }
 }
 
@@ -11,6 +14,7 @@ public class oops {
         int num2 = 10;
 
         calculate calc = new calculate();
+        calc.print();
         int result = calc.add(num1, num2);
         System.out.println(result);
     }
